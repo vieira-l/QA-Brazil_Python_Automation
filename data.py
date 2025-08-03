f4195b616625a07c867670fa72a5b55549a39a2d
vieira-l <vieiralucasat@gmail.com>
2025-08-03 19:38:13 +0100

    Subindo Projeto Sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-358c97f4-cfbd-4934-8691-9dcb3bb648bf.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
